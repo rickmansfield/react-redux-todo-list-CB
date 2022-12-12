30f2731fe331486e553a249862c987833a84e7f2
Rick Mansfield <rick.mansfield@triplecrownproducts.com>
2022-12-12 15:28:58 -0600

    Removes obsolete code

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,22 +5,12 @@ import { getTodosAsync } from '../redux/todoSlice';
 
 const TodoList = () => {
   const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
 
   useEffect(() => { 
     dispatch(getTodosAsync());
   }, [dispatch]);
-
-
-
-  const todos = useSelector((state) => state.todos);
-	// const todos = [
-	// 	{ id: 1, title: 'todo1', completed: false },
-	// 	{ id: 2, title: 'todo2', completed: false },
-	// 	{ id: 3, title: 'todo3', completed: true },
-	// 	{ id: 4, title: 'todo4', completed: false },
-	// 	{ id: 5, title: 'todo5', completed: false },
-	// ];
-
+  
 	return (
 		<ul className='list-group'>
 			{todos.map((todo) => (
