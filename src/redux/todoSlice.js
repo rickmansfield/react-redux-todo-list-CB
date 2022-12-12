fd023504ac4e0639176e49aff17944c93307cb2c
Rick Mansfield <rick.mansfield@triplecrownproducts.com>
2022-12-12 10:19:50 -0600

    Adds deleteTodo Reducer

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -21,11 +21,18 @@ const todoSlice = createSlice({
       const index = state.findIndex((todo) => todo.id === action.payload.id);
       //toggle the completed property
       state[index].completed = action.payload.completed;
+    },
+    deleteTodo: (state, action) => {
+      //find the index of the todo that matches the id of the payload
+      const index = state.findIndex((todo) => todo.id === action.payload.id);
+      //remove the todo from the array
+      state.splice(index, 1);
     }
   }
 });
 export const {
   addTodo, 
-  toggleComplete
+  toggleComplete,
+  deleteTodo
 } = todoSlice.actions;
 export default todoSlice.reducer;
