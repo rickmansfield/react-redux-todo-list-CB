28c64667b55e7d86bac13b2240b1038084fed6ab
Rick Mansfield <rick.mansfield@triplecrownproducts.com>
2022-12-12 09:41:36 -0600

    adds change handler for todo checkbox

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -5,7 +5,7 @@ const todoSlice = createSlice({
   initialState: [
     { id: 1, title: "todo1", completed: false },
     { id: 2, title: "todo2", completed: false },
-    { id: 3, title: "todo3", completed: true },
+    { id: 3, title: "todo3", completed: false },
   ],
   reducers: {
     addTodo: (state, action) => {
